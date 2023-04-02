7d7930d441976cba7d006a02f94c97aa7589fcc8
Ana Koss <anakorpusova@gmail.com>
2023-04-02 13:56:44 -0400

    day_24 #2, April 2

--- src/my_util/StringUtil.java
+++ src/my_util/StringUtil.java
@@ -2,18 +2,34 @@ package my_util;
 // reverse
 // create a method that will accept a String und returns the reversed String
 
+import java.util.Locale;
+
 /*** This method made by Loopcamp Batch#1
  * The method that will accept a String und returns the reversed String
  */
 public class StringUtil {
-    public static String reverseString ( String str){
+    public static String reverseString ( String str) {
 
         String reverse = "";
-        for (int i = str.length()-1; i >= 0; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             reverse += str.charAt(i);
         }
         return reverse;
+
     }
-
-
+    /**
+     * This method made by Loopcamp Batch#1
+     * This method accepts a String parameter and puts it is a format
+     * Ex:
+     *       The format will be like this
+     *
+     *       First letter is in upperCase and rest in lowerCase
+     *
+     * Then return a String in a formatted way.
+     */
+ public static String fixFormat(String str){
+     String fixed = str.trim();
+     fixed= str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+     return fixed;
+    }
 }
